516cf9caa657f97b96e15ba93d20110ee519a25d
Jake Kim <imjakekim@gmail.com>
2022-09-26 08:57:15 -0700

    first story

---
+++ app/controllers/doctor_controller.rb
@@ -0,0 +1,6 @@
+class DoctorController < ApplicationController
+    def show
+        @doctor = Doctor.find(params[:id])
+        @patients = @doctor.patients
+    end
+end
--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe 'doctor show page' do
     let!(:hospital1) {Hospital.create!(name: "Grey Sloan Memorial Hospital")}
     let!(:hospital2) {Hospital.create!(name: "Seaside Health & Wellness Center")}
 
-    let!(:doctor1) {hospital1.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")}
-    let!(:doctor2) {hospital2.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")}
+    let!(:doctor1) {hospital1.doctors.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University")}
+    let!(:doctor2) {hospital2.doctors.create!(name: "Alex Karev", specialty: "Pediatric Surgery", university: "Johns Hopkins University")}
 
-    let!(:patient1) {:doctor1.patients.create!(name: "Katie Bryce", age: 24)}
-    let!(:patient2) {:doctor1.patients.create!(name: "Denny Duquette", age: 39)}
-    let!(:patient3) {:doctor2.patients.create!(name: "Rebecca Pope", age: 32)}
-    let!(:patient4) {:doctor2.patients.create!(name: "Zola Shepherd", age: 2)}
+    let!(:patient1) {doctor1.patients.create!(name: "Katie Bryce", age: 24)}
+    let!(:patient2) {doctor1.patients.create!(name: "Denny Duquette", age: 39)}
+    let!(:patient3) {doctor2.patients.create!(name: "Rebecca Pope", age: 32)}
+    let!(:patient4) {doctor2.patients.create!(name: "Zola Shepherd", age: 2)}
 
     it 'shows all the doctor attributes' do
         visit doctor_path(doctor1)
         expect(page).to have_content("Doctor #{doctor1.name}")
         expect(page).to have_content("Specialty: #{doctor1.specialty}")
-        expect(page).to have_content("Univerisity: #{doctor1.education}")
-        expect(page).to have_content("of #{doctor1.hospital}")
+        expect(page).to have_content("University: #{doctor1.university}")
+        expect(page).to have_content("of #{hospital1.name}")
         expect(page).to have_content(patient1.name)
         expect(page).to have_content(patient2.name)
 
